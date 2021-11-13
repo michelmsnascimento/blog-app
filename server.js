58a1ea96066b344124acd3fe8c33e7f6fc73c439
michelmsnascimento <michelmsnascimento@gmail.com>
2021-11-13 19:44:21 -0300

    Aula 52 concluindo o CRUD

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const path = require('path') //01
 const mongoose = require('mongoose')
 const session = require("express-session")
 const flash = require("connect-flash") 
+require("./model/Postagem")
+const Postagem = mongoose.model("postagens")
+require("./model/Categoria")
+const Categoria = mongoose.model("categorias")  
 
 const server = express()
 //configurações
@@ -54,12 +58,70 @@ const server = express()
 //Rotas
 //Rota do usuario //01
 server.get('/', (req, res) => { 
-    req.send('rota principal')
+    Postagem.find().populate("categoria").lean().sort({data: "desc"}).then((postagens) => {
+        res.render("index", {postagens: postagens})
+    }).catch((err)=> {
+        req.flash("error_msg","Houve um erro interno")
+        req.redirect("/404")
+    })
+})
+server.get("/postagens/:slug", (req, res)=>{
+    Postagem.findOne({slug:req.params.slug}).lean().then((postagens) => {
+        if(postagens){
+            res.render("postagens/index", {postagens: postagens})
+        }else{
+            req.flash("error_msg","Postagem não ecxite")
+            res.redirect("/")
+        }
+    }).catch((err)=> {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/")
+    })
 })
 
-server.get('/posts', (req, res) => {
-    req.send('rota posts') 
+server.get("/categorias", (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render("categorias/index", {categorias: categorias})    
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar formulário")
+        res.redirect("/")
+    })
 })
+
+server.get("/categorias/:slug", (req, res) => {
+    Categoria.findOne({_slug: req.params.slug}).lean().then((categorias)=>{
+        if(categorias){
+
+            Postagem.find({categorias: categorias.id}).lean().then((postagens)=>{
+                res.render("categorias/postagens", {postagens: postagens, categorias: categorias})
+
+            }).catch((err) => {
+                 req.flash("error_msg", "Erro ao listar oo carregar a pagina desta categortia")
+                 req.redirect("/")
+            })
+
+        }else{
+            req.flash("error_msg", "Essa categoria não existe.")
+            res.redirect("/")
+        }
+    }).catch((err) => {
+        req.flash("error_msg", "Erro ao carregar pagina categoria")
+        req.redirect("/")
+    })
+})
+
+
+
+
+server.get("/404", (req, res) => {
+    res.send('Erro 404!')
+})
+server.get("/posts", (req, res) => {
+    res.send('Lista Posts')
+})
+
+
+
 
 //Rota do administrador routs admin.js
 server.use('/admin', admin) //01
